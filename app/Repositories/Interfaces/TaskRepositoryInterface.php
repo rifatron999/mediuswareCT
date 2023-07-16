448193f9525d494da8bc0218ff7b6d30363430ba
Kawsarul Alam Chowdhury Rifat <43786423+rifatron999@users.noreply.github.com>
2023-07-16 23:27:01 +0600

    re

--- app/Repositories/Interfaces/TaskRepositoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Repositories\Interfaces;
-
-Interface TaskRepositoryInterface{
-	
-	public function getTasks($data);
-	public function createTask($data);
-	public function updateTask($data,$id);
-}
